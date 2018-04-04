0a33a2794ab9a774b7e5d01948ee9995524b6484
Luca Pipolo <luca@netzstrategen.com>
2018-04-04 12:10:18 +0200

    Initial repo.

---
+++ shop-standards.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+  Plugin Name: Shop Standards
+  Version: 1.0.0
+  Text Domain: shop-standards
+  Description: Standard refinements for e-commerce websites.
+  Author: netzstrategen
+  Author URI: http://www.netzstrategen.com
+  License: GPL-2.0+
+  License URI: http://www.gnu.org/licenses/gpl-2.0
+*/
