b1c3418b50cc0a7097a714d34f6828e18bf2fa96
Joan López <joan@netzstrategen.com>
2018-11-13 16:48:04 +0100

    Added WP CLI command to update products sale discount percentage.

---
+++ src/CliCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\ShopStandards\CliCommand.
+ */
+
+namespace Netzstrategen\ShopStandards;
+
+use WP_CLI;
+
+/**
+ * WP-CLI commands implementation.
+ */
+class CliCommand extends \WP_CLI_Command {
+
+  public function refreshSalePercentage(array $args, array $options) {
+    $options = wp_parse_args($options, ['all' => '']);
+    if ($options['all']) {
+      $product_ids = wc_get_products([
+        'return' => 'ids',
+        'limit' => -1,
+      ]);
+      static::updateSalePercentage($product_ids);
+      WP_CLI::success('Sale percentage have been sucessfully updated.');
+    }
+    elseif (isset($args[0])) {
+      $product_ids = explode(',', trim($args[0], ','));
+      static::forceUpdateProducts($product_ids);
+    }
+    else {
+      WP_CLI::error('A comma separated list of products ids is needed. Use option --all to update all products.');
+    }
+  }
+
+  /**
+   * Forces the update of products sale discounted percentage.
+   *
+   * @param array $product_ids
+   *   List of product unique identifiers.
+   */
+  public static function updateSalePercentage($product_ids) {
+    foreach ($product_ids as $product_id) {
+      try {
+        WooCommerce::updateSalePercentage(FALSE, $product_id, '_sale_price', FALSE);
+      }
+      catch (\Exception $e) {
+        WP_CLI::error($e->getMessage());
+        exit();
+      }
+    }
+  }
+
+}
