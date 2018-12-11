62cca123d8ff3cf8b2f12c0d5ac780cbae7acd58
Joan López <joan@netzstrategen.com>
2018-12-11 18:07:46 +0100

    Added gulp-task-collection to plugin shop-standards.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.6.0
+  Version: 1.7.0
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
