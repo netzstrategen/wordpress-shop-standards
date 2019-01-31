a49deb52489131620c8888ba1c281e1db20d0f39
Joan López <juanlopez4691@gmail.com>
2019-01-31 11:34:12 +0100

    Fixed wp-cli refreshSalePercentage command handling of parameters.

--- src/CliCommand.php
+++ src/CliCommand.php
@@ -18,6 +18,7 @@ class CliCommand extends \WP_CLI_Command {
    * Updates products sale discounted percentage.
    */
   public function refreshSalePercentage(array $args, array $options) {
+    $options = wp_parse_args($options, ['all' => '']);
     if ($options['all']) {
       $product_ids = wc_get_products([
         'return' => 'ids',
@@ -31,7 +32,7 @@ class CliCommand extends \WP_CLI_Command {
       static::updateSalePercentage($product_ids);
     }
     else {
-      WP_CLI::error('A comma separated list of products ids is needed. Use option --all to update all products.');
+      WP_CLI::error('A comma separated list of products IDs is needed. Use option --all to update all products.');
     }
   }
 
@@ -43,8 +44,12 @@ class CliCommand extends \WP_CLI_Command {
    */
   public static function updateSalePercentage($product_ids) {
     foreach ($product_ids as $product_id) {
+      if (!$product_id = is_numeric($product_id) && (int) $product_id == $product_id ? (int) $product_id : 0) {
+        WP_CLI::error('Invalid product ID.');
+        exit();
+      }
       try {
-        WooCommerce::updateSalePercentage(FALSE, $product_id, '_sale_price', FALSE);
+        Admin::updateSalePercentage(FALSE, $product_id, '_sale_price', FALSE);
       }
       catch (\Exception $e) {
         WP_CLI::error($e->getMessage());
