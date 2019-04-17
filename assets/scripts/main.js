f2c975713072bf8241b210857ea6a74b5232b828
Joan López <joan@netzstrategen.com>
2019-04-17 12:10:09 +0200

    fixup! Fixed disabled second checkout page allowed repeated orders to be created.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,10 +5,6 @@
    * If the page takes time to be loaded, the user could click multiple
    * times on the place order button and this would generate multiple orders.
    * To prevent this we stop form submit event propagation after the first click.
-   *
-   * Covers the case of woocommerce-german-market second checkout page being
-   * disabled. If form fields validation fails, the order submit button is
-   * enabled again to allow the user to resend the form.
    */
 
   const placeOrderBtnInitialValue = $('#place_order').prop('value');
