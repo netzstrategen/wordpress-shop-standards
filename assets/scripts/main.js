acaa7dcafaa617eb38e7d242b905d44bc2fb6e05
Joan López <joan@netzstrategen.com>
2019-04-17 11:03:06 +0200

    fixup! Fixed disabled second checkout page allowed repeated orders to be created.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,30 +5,21 @@
    * If the page takes time to be loaded, the user could click multiple
    * times on the place order button and this would generate multiple orders.
    * To prevent this we stop form submit event propagation after the first click.
+   *
+   * Covers the case of woocommerce-german-market second checkout page being
+   * disabled. If form fields validation fails, the order submit button is
+   * enabled again to allow the user to resend the form.
    */
-  $('form.checkout.wgm-second-checkout').submit((e) => {
-    if ($('#place_order').hasClass('disabled')) {
-      e.preventDefault();
-      return;
-    }
-    if ($('#terms').prop('checked') === true && $('#shipping-service-provider').prop('checked') === true) {
-      $('#place_order').prop('value', 'Ihre Bestellung wird jetzt verarbeitet…').addClass('disabled');
-    }
-  });
 
-  /**
-   * Prevents multiple order to be sent even if woocommerce-german-market second
-   * checkout page is disabled. If form fields validation fails, the order
-   * submit button is enabled again to allow the user to resend the form.
-   */
-  $('#place_order').click(function (e) {
+  const placeOrderBtnInitialValue = $('#place_order').prop('value');
+  $('#place_order').click(function disableSubmitOrder(e) {
     if ($(this).hasClass('disabled')) {
       e.preventDefault();
       return;
     }
-    $(this).addClass('disabled');
+    $('#place_order').prop('value', 'Ihre Bestellung wird jetzt verarbeitet…').addClass('disabled');
   });
-  $(document.body).on('checkout_error', (data) => {
-    $('#place_order').removeClass('disabled');
+  $(document.body).on('checkout_error', () => {
+    $('#place_order').prop('value', placeOrderBtnInitialValue).removeClass('disabled');
   });
 }(jQuery));
