48a449a531ced1e0d11e5fa8b471f98d49eaad5a
Joan López <joan@netzstrategen.com>
2019-05-03 16:21:20 +0200

    Added WP CLI command to update products delivery time.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.21.6
+  Version: 1.22.0
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
