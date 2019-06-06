a8a4a8eea3760c7eb04e3640f652bc9cbdb4f6be
Joan López <joan@netzstrategen.com>
2019-06-06 11:50:57 +0200

    Added automatic selection of product variation attributes when only one option available. (#37)

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.23.3
+  Version: 1.24.0
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
