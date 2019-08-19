401748ae59ad6e8b2a3cb49b9d4051afaf94ed6e
Fabian Marz <fabian@netzstrategen.com>
2019-08-19 14:50:51 +0200

    bump version number

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.24.2
+  Version: 1.24.3
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
