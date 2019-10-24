74fbbbf9399d0393fba62d68caed0ead378d629c
Evelia Molina <evelia@netzstrategen.com>
2019-10-24 13:18:29 +0200

    Fixed missing gtin product number for single product pages. (#51)

--- src/Schema.php
+++ src/Schema.php
@@ -25,4 +25,37 @@ class Schema {
   public static function uninstall() {
   }
 
+  /**
+   * Retrieves GTIN product number and sets GTIN property name.
+   *
+   * @implements woocommerce_structured_data_product
+   */
+  public static function get_product_gtin($data) {
+    global $product;
+
+    if (!$gtin = get_post_meta($product->get_id(), '_' . Plugin::PREFIX . '_gtin', TRUE)) {
+      return $data;
+    }
+
+    switch (strlen(trim($gtin))) {
+      case 8:
+        $gtin_format_type = 'gtin8';
+        break;
+
+      case 13:
+        $gtin_format_type = 'gtin13';
+        break;
+
+      case 14:
+        $gtin_format_type = 'gtin14';
+        break;
+
+      default:
+        $gtin_format_type = 'gtin12';
+    }
+
+    $data[$gtin_format_type] = $gtin;
+    return $data;
+  }
+
 }
