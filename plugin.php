11e131da9ca3aca4c0a3f0536f84f26a96915901
Luca Pipolo <luca@netzstrategen.com>
2019-11-15 12:29:13 +0100

    Fixed some countries postal code were not validated. (#62)

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.28.1
+  Version: 1.29.0
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
