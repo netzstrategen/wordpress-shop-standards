4294fa5ed210ba886c64f785d102bc755a7d0b84
Joan López <joan@netzstrategen.com>
2019-11-15 14:41:10 +0100

    Fixed PHP error and minor code cleanup. (#64)

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.29.0
+  Version: 1.29.1
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
