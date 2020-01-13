506ac9597d7d19b8fb674cebbd1430b788919731
Luca Pipolo <luca@netzstrategen.com>
2020-01-13 08:42:46 +0100

    fixup! Fixed sale category was not correctly assigned on product update.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.30.4
+  Version: 1.30.5
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
