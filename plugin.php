3b03e89170b978665e9d557713664fff2065f8c3
Joan López <joan@netzstrategen.com>
2020-03-06 17:49:25 +0100

    fixup! Fixed PHP notice when trying to defer enqueuing of scripts.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.30.13
+  Version: 1.30.14
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
