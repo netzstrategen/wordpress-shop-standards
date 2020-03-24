04a12f065008b8bc5220325fa823cf3c8333641a
Moritz Bappert <info@moritzbappert.de>
2020-03-24 10:34:04 +0100

    Fixed conflicts left in language files.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.30.16
+  Version: 1.30.17
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
