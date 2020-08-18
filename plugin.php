eb9e2beb09b8f1e16f4abd947e157af8b949b6b1
Luca Pipolo <luca@netzstrategen.com>
2020-08-18 12:44:15 +0200

    Fixed confirmation email field wrong position.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.35.4
+  Version: 1.35.5
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
