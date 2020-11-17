a526c165ce9573da6205f5663d8e0c475208b127
Daniel Kudwien <daniel@netzstrategen.com>
2020-11-17 15:50:39 +0100

    fixup docs

--- src/Admin.php
+++ src/Admin.php
@@ -37,10 +37,8 @@ class Admin {
     // Enqueues admin plugin scripts.
     add_action('admin_enqueue_scripts', __CLASS__ . '::admin_enqueue_scripts');
 
-    // Removes woocommerce admin notices.
+    // Removes admin notices about updating to version and connecting to WooCommerce.com.
     add_filter('woocommerce_helper_suppress_admin_notices', '__return_true');
-
-    // Removes only woocommerce connect related admin notices.
     add_filter('woocommerce_helper_suppress_connect_notice', '__return_true');
   }
 
