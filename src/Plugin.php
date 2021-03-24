6f5827d36e44fd81206742068f513c5e77688f2b
Tom Hare <colourgarden@users.noreply.github.com>
2021-03-24 16:27:28 +0100

    Fixed variations add to cart button not hidden when custom option set. (#137)
    Custom "Hide add to cart button" (added in 690d6c) works for single products but not variations.
    Button is changed via JS so not feasible to remove completely via PHP.
    'disabled' class added to button is used in JS to prevent purchases so CSS-only hide here is ok.

--- src/Plugin.php
+++ src/Plugin.php
@@ -288,6 +288,7 @@ class Plugin {
   public static function wp_enqueue_scripts() {
     $git_version = static::getGitVersion();
 
+    wp_enqueue_style(Plugin::PREFIX, static::getBaseUrl() . '/dist/styles/main.min.css', FALSE, $git_version);
     wp_enqueue_script(Plugin::PREFIX, static::getBaseUrl() . '/dist/scripts/main.min.js', ['jquery'], $git_version, TRUE);
     wp_localize_script(Plugin::PREFIX, 'shop_standards_settings', [
       'emailConfirmationEmail' => get_option('_' . Plugin::L10N . '_checkout_email_confirmation_field'),
