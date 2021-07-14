383916f80fdbc3cf4488eeeda3ff5001f272826f
Mauricio Urrego <mauricio@netzstrategen.com>
2021-07-14 15:03:01 +0200

    fixup! Fixed fatal errors if acf is not installed.

--- src/ProductAttributePageTitle.php
+++ src/ProductAttributePageTitle.php
@@ -20,9 +20,7 @@ class ProductAttributePageTitle {
     }
 
     // Register ACF field.
-    if (function_exists('register_field_group')) {
-      ProductAttributePageTitle::register_acf_page_title($group_filter);
-    }
+    ProductAttributePageTitle::register_acf_page_title($group_filter);
 
     // Front-end output of title.
     add_filter('woocommerce_page_title', __CLASS__.'::woocommerce_page_title');
@@ -32,30 +30,32 @@ class ProductAttributePageTitle {
    * Register ACF field.
    */
   public static function register_acf_page_title($group_filter) {
-    register_field_group([
-      'key' => 'acf_group_page_title',
-      'title' => __('Listing Page', Plugin::L10N),
-      'fields' => [
-        [
-          'key' => 'acf_field_page_title',
-          'label' => __('Page Title', Plugin::L10N),
-          'name' => 'page_title',
-          'type' => 'text',
-          'instructions' => __('Only changes how the name appears on the page. Leaves the filter drop-down menu alone.', Plugin::L10N),
-          'required' => 0,
-          'conditional_logic' => 0,
+    if (function_exists('register_field_group')) {
+      register_field_group([
+        'key' => 'acf_group_page_title',
+        'title' => __('Listing Page', Plugin::L10N),
+        'fields' => [
+          [
+            'key' => 'acf_field_page_title',
+            'label' => __('Page Title', Plugin::L10N),
+            'name' => 'page_title',
+            'type' => 'text',
+            'instructions' => __('Only changes how the name appears on the page. Leaves the filter drop-down menu alone.', Plugin::L10N),
+            'required' => 0,
+            'conditional_logic' => 0,
+          ],
         ],
-      ],
-      'location' => $group_filter,
-      'menu_order' => 0,
-      'position' => 'normal',
-      'style' => 'default',
-      'label_placement' => 'top',
-      'instruction_placement' => 'label',
-      'hide_on_screen' => '',
-      'active' => true,
-      'description' => '',
-    ]);
+        'location' => $group_filter,
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+      ]);
+    }
   }
 
   /**
