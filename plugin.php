706994e2d04cdd78d440f83da611f0475d4de55b
Bogdan Arizancu <bogdan@netzstrategen.com>
2021-07-28 09:43:21 +0300

    Bumped version to 1.40.2.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.40.1
+  Version: 1.40.2
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
