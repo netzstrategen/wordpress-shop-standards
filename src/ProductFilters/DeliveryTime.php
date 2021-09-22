54eef475c4d2d0ae633589573c3373867d96f5d8
Bogdan Arizancu <bogdan@netzstrategen.com>
2021-09-22 15:51:04 +0200

    Updated version to 1.40.5.

--- src/ProductFilters/DeliveryTime.php
+++ src/ProductFilters/DeliveryTime.php
@@ -45,16 +45,27 @@ class DeliveryTime {
       return;
     }
     $filter_values = array_filter(array_map('absint', explode(',', wp_unslash($filter_values))));
-    $meta_query = [[
-      'key' => '_lieferzeit',
-      'value' => $filter_values,
-      'compare' => 'IN',
-    ],
-    [
-      'key' => '_stock_status',
-      'value' => 'instock',
-      'compare' => '='
-    ]];
+    $meta_query = [
+      'relation' => 'AND',
+      [
+        'relation' => 'OR',
+        [
+          'key' => '_shop-standards_back_in_stock_date',
+          'compare' => 'NOT EXISTS',
+        ],
+        [
+          'key' => '_shop-standards_back_in_stock_date',
+          'value' => date('Y-m-d'),
+          'compare' => '<=',
+        ]
+      ],
+      [
+        'key' => '_lieferzeit',
+        'value' => $filter_values,
+        'compare' => 'IN',
+      ]
+    ];
+
     $query->set('meta_query', $meta_query);
   }
 
