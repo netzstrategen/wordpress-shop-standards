d17baeeaf8a54ffcb8bf33f81121fa4282a00d9f
Christian <christian@netzstrategen.com>
2021-11-22 12:13:55 +0100

    Added comments

--- src/Products.php
+++ src/Products.php
@@ -3,7 +3,7 @@
 namespace Netzstrategen\ShopStandards;
 
 /**
- *
+ * Meant to handle special Product features.
  */
 class Products {
 
@@ -13,13 +13,18 @@ class Products {
 
   const FIELD_ENFORCE_CAT_LINKS = Plugin::PREFIX . '_enforce_main_category_links';
 
+  /**
+   * Stores the product base link.
+   *
+   * @var string
+   */
   private string $product_base_link;
 
   /**
-   *
+   * Initialize the product module.
    */
   public static function init(): void {
-    $obj = new self;
+    $obj = new self();
     if (get_option(self::FIELD_ENFORCE_CAT_LINKS)) {
       add_filter('post_type_link', [$obj, 'get_product_permalink'], 10, 2);
     }
@@ -27,6 +32,9 @@ class Products {
     add_action('admin_init', [$obj, 'save_product_settings']);
   }
 
+  /**
+   * Registers custom product settings.
+   */
   public function register_product_settings(): void {
     add_settings_field(self::FIELD_ENFORCE_CAT_LINKS,
       __('Enforce main category on product links', Plugin::L10N),
@@ -35,6 +43,9 @@ class Products {
       'woocommerce-permalink');
   }
 
+  /**
+   * Persists custom product settings.
+   */
   public function save_product_settings(): void {
     if ($this->is_saving_settings()) {
       $option_checked = $_POST[self::FIELD_ENFORCE_CAT_LINKS] ?? 0;
@@ -43,22 +54,28 @@ class Products {
   }
 
   /**
+   * Determines if the custom settings should be stored.
+   *
    * @return bool
+   *   TRUE if the current screen is correct.
    */
   private function is_saving_settings(): bool {
     return !empty($_POST) && strpos($_SERVER['REQUEST_URI'],
         'options-permalink') !== FALSE;
   }
 
+  /**
+   * Renders custom option in page.
+   */
   public function render_permalink_option() { ?>
       <input name="<?= self::FIELD_ENFORCE_CAT_LINKS ?>" type="checkbox"
              value="1" <?php
-      checked(get_option(self::FIELD_ENFORCE_CAT_LINKS)) ?> />
+              checked(get_option(self::FIELD_ENFORCE_CAT_LINKS)) ?> />
     <?php
   }
 
   /**
-   *
+   * Ensures the product link contains the main category.
    */
   public function get_product_permalink(
     string $post_link,
@@ -96,6 +113,9 @@ class Products {
     return home_url($product_link);
   }
 
+  /**
+   * Retrieves the current prodyct base link to be replaced.
+   */
   public function get_product_base_link(string $cat_placeholder): string {
     if (isset($this->product_base_link)) {
       return $this->product_base_link;
