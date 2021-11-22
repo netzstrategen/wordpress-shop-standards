70fd50aec5fff128b409392192d947d3fcd7b736
Christian <christian@netzstrategen.com>
2021-11-22 12:21:26 +0100

    Fixed indentiation

--- src/Plugin.php
+++ src/Plugin.php
@@ -112,7 +112,7 @@ class Plugin {
     WooCommerceSalutation::init();
     WooCommerceCheckout::init();
     PlusProducts::init();
-	  Products::init();
+    Products::init();
 
     if (is_admin()) {
       return;
