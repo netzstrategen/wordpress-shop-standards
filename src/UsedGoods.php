69ad12e94bec1a5a82fd6ec1523c1a74cfd6c8d3
Mauricio Urrego <mauricio@netzstrategen.com>
2021-12-22 12:25:56 +0100

    Initial commit.

---
+++ src/UsedGoods.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\ShopStandards\UsedGoods.
+ */
+
+namespace Netzstrategen\ShopStandards;
+
+/**
+ * Used or defective goods user consent checkbox functionality.
+ */
+class UsedGoods {
+
+  /**
+   * Checkbox consent initialization method.
+   */
+  public static function init() {
+    var_dump('test');
+  }
+
+}
