0efeae36529d7cf3fcf85028d618ee6b2a3047e8
Mauricio Urrego <mauricio@netzstrategen.com>
2021-12-22 13:07:35 +0100

    Added agreement text, checkbox input, and inital styling.

--- src/UsedGoods.php
+++ src/UsedGoods.php
@@ -23,7 +23,18 @@ class UsedGoods {
    * Displays the checkbox and agreement text.
    */
   public static function displayCheckbox() {
-    echo '<div>Checkbox</div>';
+    $test = 'test attribute';
+    echo '<div class="checkbox-container">
+      <b>Hinweis und gesonderte Vereinbarung zu den Abweichungen von den objektiven Anforderungen</b>
+      <p>Hinweis: Bitte beachten Sie, dass Sie die Ware nicht in den Warenkorb legen können, wenn Sie mit der Vereinbarung nicht einverstanden sind. Ihr Einverständnis mit der Vereinbarung erklären Sie durch Anklicken der Checkbox.</p>
+      <div class="checkbox-text-container">
+        <input class="checkbox-box" type="checkbox" id="used-goods-consent" name="used-goods-consent" value="used-goods-consent-submit">
+        <div class="checkbox-text">
+          <p>Es wird vereinbart, dass die hier angebotene Ware in den folgenden Merkmalen in ihrer Beschaffenheit von den objektiven Anforderungen abweicht und ein Mängelgewährleistungsrecht diesbezüglich ausgeschlossen ist:</p>
+          <p>-(z.B.: ' . $test . ')</p>
+        </div>
+      </div>
+    </div>';
   }
 
 }
