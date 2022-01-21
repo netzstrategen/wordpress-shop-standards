22370c09e835de4a2a93dd33b12bf2e15e5eccd8
Christian <christian@netzstrategen.com>
2022-01-21 12:05:44 +0100

    fix from review

--- src/ProductDefects.php
+++ src/ProductDefects.php
@@ -38,8 +38,6 @@ class ProductDefects {
     }
     if (self::get_display_product_attribute(wc_get_product())) {
       add_action('woocommerce_before_add_to_cart_button', __CLASS__ . '::woocommerce_before_add_to_cart_button');
-      // Logs consent to defective or used product when product is added to cart.
-      add_action('woocommerce_add_to_cart', __CLASS__ . '::log_consent_file');
     }
   }
 
@@ -61,32 +59,6 @@ class ProductDefects {
     // Returns value when is not found in the product attrs as expected.
     // But also when is a variable product which will be handled via JS.
     return $product->is_type('variable') || strpos($attr_value, $tax_value) === FALSE ? $attr_value : NULL;
-  }
-
-  /**
-   * Logs consent to defective or used product.
-   *
-   * @implemements woocommerce_add_to_cart
-   */
-  public static function log_consent_file() {
-    if (!isset($_POST['used-goods-consent'])) {
-      return;
-    }
-
-    $uploads_dir = wp_upload_dir()['basedir'] . '/' . Plugin::PREFIX;
-    $log_file = $uploads_dir . '/defects-consent.log';
-    if (!dir($uploads_dir)) {
-      wp_mkdir_p($uploads_dir);
-    }
-    $data = [
-      'timestamp' => date_i18n('c'),
-      'user' => get_current_user_id(),
-      'referrer' => $_SERVER['HTTP_REFERER'],
-      'consent' => $_POST['used-goods-consent'],
-      'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? '',
-    ];
-    $data = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-    file_put_contents($log_file, $data . "\n", FILE_APPEND);
   }
 
   /**
@@ -122,7 +94,7 @@ class ProductDefects {
   }
 
   /**
-   * Adds checkout specific settings.
+   * Adds specific settings under shop-standards section.
    *
    * @implements woocommerce_get_settings_shop_standards
    */
