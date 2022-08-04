938453e8025272e6e5195eb29c314e15568320ab
danielistrate27 <daniel.istrate@olivestudio.ro>
2022-08-04 15:43:08 +0300

    Added Divers option for salutation field.

--- src/WooCommerceSalutation.php
+++ src/WooCommerceSalutation.php
@@ -65,6 +65,7 @@ class WooCommerceSalutation {
         'Mrs' => __('Mrs', Plugin::L10N),
         'Mr' => __('Mr', Plugin::L10N),
         'Company' => __('Company', Plugin::L10N),
+        'Divers' => __('Divers', Plugin::L10N),
       ],
       'priority' => 5,
     ];
