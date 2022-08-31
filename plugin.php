cce1b56238dd24f73602c6f0f07ba55ed0281a3e
Daniel Kudwien <daniel@netzstrategen.com>
2022-08-31 14:19:23 +0200

    bump version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Standards
-  Version: 1.44.3
+  Version: 1.45.0
   Text Domain: shop-standards
   Description: Standard refinements for e-commerce websites.
   Author: netzstrategen
