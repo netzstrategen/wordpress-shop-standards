47c57722f8529632eb3b53ddf3e35ed6d7a78c04
navidabdi <artsanaat@gmail.com>
2022-10-31 11:04:13 +0300

    Fix/Invalidated object cashe in woocommerce-order-status-manager plugin.

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -1514,14 +1514,14 @@ class WooCommerce {
     return $dropdown_values;
   }
 
-   /**
+  /**
    * Invalidates the object cache in Woocommerce order status manager.
-   * 
+   *
    * @implements save_post_wc_order_status
-   * 
    */
   public static function save_post_wc_order_status() {
-    if (wp_cache_get('wc_order_status_manager_order_status_posts')) {
+    wp_cache_get('wc_order_status_manager_order_status_posts', '', FALSE, $found);
+    if ($found) {
       wp_cache_delete('wc_order_status_manager_order_status_posts');
     }
   }
