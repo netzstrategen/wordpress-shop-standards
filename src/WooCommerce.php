21da676b525b6c0325fa64774e986928664e0e5b
navidabdi <artsanaat@gmail.com>
2022-10-31 12:05:06 +0300

    Fix/Added split package info in email orders.

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -27,8 +27,6 @@ class WooCommerce {
    */
   public static function init(): void {
     add_filter(Plugin::PREFIX . '/display_custom_product_fields', __CLASS__ . '::get_product_fields');
-    add_filter('woocommerce_get_order_item_totals', __CLASS__ . '::woocommerce_get_order_item_totals', 10, 3);
-
   }
 
   /**
@@ -1513,56 +1511,6 @@ class WooCommerce {
     ];
 
     return $dropdown_values;
-  }
-
-  /**
-   * Calculates shipping price + tax
-   *
-   * @param  object $shipping_method
-   * @return string
-   */
-  public static function getTotalShippingPrice($shipping_method):string {
-    $currency = ' ' . get_woocommerce_currency_symbol();
-    $shipping_price = floatval($shipping_method->get_total());
-
-    if(!$shipping_price) return '<br />';
-
-    $shipping_price_tax = floatval($shipping_method->get_total_tax());
-    $shipping_price_total = $shipping_price + $shipping_price_tax;
-    $shipping_price_total_with_symbol = ' - ' . $shipping_price_total . $currency . '<br />';
-
-    return $shipping_price_total_with_symbol;
-  }
-
-  /**
-   * Separates the shipping methods in new lines in emails.
-   *
-   * @param  array $total_rows
-   * @param \WC_Order $order
-   * @return array
-   */
-  public static function woocommerce_get_order_item_totals($total_rows, \WC_Order $order, $tax_display):array {
-    $shipping_methods = $order->get_shipping_methods();
-
-    if(count($shipping_methods) > 1) {
-
-      $shipping_methods_row = '';
-      $count = 1;
-
-      foreach($shipping_methods as $shipping_method) {
-        $shipping_price_total_with_symbol = self::getTotalShippingPrice($shipping_method);
-
-        $positionen = $shipping_method->get_meta("Positionen") ?? '';
-        $shipping_methods_row .= "($count) ";
-        $shipping_methods_row .= '<strong>' . $shipping_method->get_name() . '</strong>';
-        $shipping_methods_row .= $positionen ? ': ' . $positionen : '';
-        $shipping_methods_row .= $shipping_price_total_with_symbol;
-        $count++;
-      }
-
-      $total_rows['shipping']['value'] = $shipping_methods_row;
-    }
-    return $total_rows;
   }
 
 }
---
+++ src/WooCommerceShippingPackages.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @file
+ * Contains \Netzstrategen\ShopStandards\WooCommerceShippingPackages.
+ */
+
+namespace Netzstrategen\ShopStandards;
+
+/**
+ * Shipping related settings and actions.
+ */
+class WooCommerceShippingPackages {
+
+  /**
+   * Initialize the needed functions and hooks.
+   */
+  public static function init() {
+    // Show multiple shipping methods separately in order emails.
+    add_filter('woocommerce_get_order_item_totals', __CLASS__ . '::woocommerce_get_order_item_totals', 10, 2);
+  }
+
+  /**
+   * Calculates the shipping costs including tax.
+   *
+   * @param \WC_Order_Item_Shipping $shipping_method
+   *   A shipping item object derived from a WC_Order.
+   *
+   * @return string
+   *   The shipping costs including taxes, currency sign, and HTML line-breaks,
+   *   for output in the HTML order emails.
+   */
+  public static function getShippingCostsIncludingTaxes(\WC_Order_Item_Shipping $shipping_method): string {
+
+    if ($shipping_price) {
+      $currency = ' ' . get_woocommerce_currency_symbol();
+      $shipping_price = floatval($shipping_method->get_total());
+      $shipping_price_tax = floatval($shipping_method->get_total_tax());
+      $shipping_price_total = $shipping_price + $shipping_price_tax;
+      $shipping_price_total_with_symbol = ' - ' . $shipping_price_total . $currency . '<br />';
+
+      return $shipping_price_total_with_symbol;
+    }
+
+    return '<br />';
+  }
+
+  /**
+   * Separates the shipping methods in new lines in emails.
+   *
+   * @param array $total_rows
+   *   Total rows of woocommerce order items.
+   * @param \WC_Order $order
+   *   A Wc_Order object.
+   *
+   * @return array
+   *   The shipping details including shipping method name, shipping costs,
+   *   taxes and currency sign.
+   */
+  public static function woocommerce_get_order_item_totals($total_rows, \WC_Order $order): array {
+    $shipping_methods = $order->get_shipping_methods();
+
+    if (count($shipping_methods) > 1) {
+
+      $shipping_methods_row = '';
+      $count = 1;
+
+      foreach ($shipping_methods as $shipping_method) {
+        $shipping_price_total_with_symbol = self::getShippingCostsIncludingTaxes($shipping_method);
+
+        $items = $shipping_method->get_meta('Positionen') ?? '';
+        $shipping_methods_row .= "($count) ";
+        $shipping_methods_row .= '<strong>' . $shipping_method->get_name() . '</strong>';
+        $shipping_methods_row .= $items ? ': ' . $items : '';
+        $shipping_methods_row .= $shipping_price_total_with_symbol;
+        $count++;
+      }
+
+      $total_rows['shipping']['value'] = $shipping_methods_row;
+    }
+    return $total_rows;
+  }
+
+}
